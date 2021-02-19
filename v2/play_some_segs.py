7472a91ddb70e5f92fa868d7c46d65f796db2d76
Dimitri Kanevsky <dkanevsky@google.com>
2021-02-19 11:36:13 -0500

    testing functionality, dialogue box

--- v2/play_some_segs.py
+++ v2/play_some_segs.py
@@ -2,6 +2,8 @@ import argparse
 import os
 from random import shuffle
 import subprocess, sys
+import tkinter as tk
+from tkinter import simpledialog
 
 parser = argparse.ArgumentParser(description='Process range.')
 parser.add_argument('--range', '-r', type=int, nargs=2,
@@ -33,9 +35,30 @@ idxs = []
 for i in range(args.range[0], args.range[1]):
     idxs.append(i)
 
+ROOT = tk.Tk()
+
+ROOT.withdraw()
+
+padding = 50 * " "
+
 segs = []
+preds = []
 for root,dirs,video_segments in os.walk('./segments/'):
     for i in idxs:
             # subprocess.call([opener, 'segments/IMG_4654-{}-subs.mp4'.format(i)])
-        segs.append('segments/IMG_4654-{}-subs.mp4'.format(i))      
-    subprocess.call(["mpv", '--fs', '--loop-file=inf'] + segs)      
+        segs.append('segments/IMG_4654-{}-subs.mp4'.format(i))
+    # subprocess.call(["mpv", '--fs', '--loop-file=inf'] + segs)    
+
+    for i in idxs:
+        subprocess.call(["mpv", '--fs', '--sid=no'] + ['segments/IMG_4654-{}-subs.mp4'.format(i)])
+        inp = simpledialog.askstring(title="Enter word guess for segment {}: \n".format(i), prompt="What did Sara just say?" + padding)
+        with open('segments/IMG_4654-seg-{}.srt'.format(i)) as f:
+            contents = f.read()
+            print(contents)
+            if inp and not inp.isspace() and (inp in contents or inp.lower() in contents or inp.capitalize() in contents):
+                preds.append(1)
+            else:
+                preds.append(0)
+print(preds)
+# calculate and print WER
+print("WER (% of correctly guessed words): {}".format(len([i for i in preds if i == 1]) / len(preds)))
