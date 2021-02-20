fe94d3afe51a4c3647d3bdb6b5d04d0a50a9bc16
Dimitri Kanevsky <dkanevsky@google.com>
2021-02-20 12:29:09 -0800

    speech rec version of lip reading/ haptics/ etc test

---
+++ v2/play_some_segs_sr.py
@@ -0,0 +1,106 @@
+import argparse
+import os
+from random import shuffle
+import speech_recognition as sr
+import subprocess, sys
+import tkinter as tk
+from tkinter import simpledialog
+from tkinter import *
+
+class MyDialog(simpledialog.Dialog):
+    def body(self, master):
+        self.geometry("800x600")
+        tk.Label(master, text="What did Sara just say?").grid(row=0)
+
+        self.e1 = tk.Entry(master)
+        self.e1.grid(row=0, column=1)
+        return self.e1 # initial focus
+
+    def apply(self):
+        first = self.e1.get()
+        self.result = first
+
+
+parser = argparse.ArgumentParser(description='Process range.')
+parser.add_argument('--range', '-r', type=int, nargs=2,
+                    help='start and end integer range of videos to select. In form start end')
+
+args = parser.parse_args()
+
+# num_segs = len([name for name in os.listdir('./segments/') if os.path.isfile(os.path.join('./segments/', name))])
+
+opener ="open" if sys.platform == "darwin" else "xdg-open"
+
+closer ="close" if sys.platform == "darwin" else "xdg-close"
+
+'''
+rand_list = []
+for i in range(5):
+    rand_list.append(randrange(num_segs))
+'''
+
+
+'''
+rand_list = []
+for i in range(args.range[0], args.range[1]):
+    rand_list.append(i)
+shuffle(rand_list)
+'''
+
+idxs = []
+for i in range(args.range[0], args.range[1]):
+    idxs.append(i)
+
+def pop_up():
+    ROOT = tk.Tk()
+    ROOT.option_add('*font', 'Helvetica -30')
+    Window = Frame(ROOT)
+    TextWidget = Text(Window)
+    TextWidget.pack()
+    Window.pack()
+    ROOT.focus_force() # This is for linux, comment out following line
+    # Window.after(1, lambda: Window.focus_force()) # This is for OSX, comment out above line
+    TextWidget.focus_set()
+    ROOT.withdraw()
+    return ROOT
+
+# segs = []
+preds = []
+
+# for root,dirs,video_segments in os.walk('./segments/'):
+    # for i in idxs:
+            # subprocess.call([opener, 'segments/IMG_4654-{}-subs.mp4'.format(i)])
+        # segs.append('segments/IMG_4654-{}-subs.mp4'.format(i))
+    # subprocess.call(["mpv", '--fs', '--loop-file=inf'] + segs)    
+
+for i in idxs:
+    spoken_answer = None
+    subprocess.call(["mpv", '--fs', '--sid=no'] + ['segments/IMG_4654-{}-subs.mp4'.format(i)])
+    # ROOT = pop_up()
+    r = sr.Recognizer()
+    m = sr.Microphone()
+
+    while spoken_answer is None:
+        with m as source:
+            r.adjust_for_ambient_noise(source)
+            print('What did Sara say?')
+            audio = r.listen(source)
+            try:
+                spoken_answer = r.recognize_google(audio)
+            except:
+                continue
+
+    inp = spoken_answer
+
+    print(inp)
+
+    with open('segments/IMG_4654-seg-{}.srt'.format(i)) as f:
+        contents = f.read()
+        print(contents)
+        if inp and not inp.isspace() and (inp in contents or inp.lower() in contents or inp.capitalize() in contents):
+            preds.append(1)
+        else:
+            preds.append(0)
+print(preds)
+# calculate and print WER
+print("WER (% of correctly guessed words): {}".format(len([i for i in preds if i == 1]) / len(preds)))
