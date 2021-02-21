b21de92dace8ffb4b3c3efe33ba5b3aeaef0d141
Galina Kanevsky <galinakanevsky@Galinas-iMac.home>
2021-02-21 13:28:12 -0500

    if MOV file exists dont download else download from gdrive

--- v2/main.sh
+++ v2/main.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+if test -f "IMG_4654.MOV"; then
+    echo "IMG_4654.MOV exists, skipping download."
+else
+    gdown https://drive.google.com/uc?id=1_YWTi4gTOS6e5AKuGXWopAMUPDFU3IRl
+fi
 mkdir segments
 # python3 rand_seg.py
 python3 split_vid.py -f IMG_4654.MOV -t vibrotablecp.txt
